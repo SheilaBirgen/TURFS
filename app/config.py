02699970f7db1885529e442929956b7a58e78283
Murutu <murutupeter@email.com>
2020-02-20 15:32:21 +0300

    imported flask_script

--- app/config.py
+++ app/config.py
@@ -14,5 +14,23 @@ class Config:
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
     
+class ProdConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
     
+class DevConfig(Config):
+    '''
+    Development configuration child class
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''  
+    
+    SECRET_KEY = 'tuf'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://peter:ozil@localhost/turfs'
+    DEBUG = True
+    ENV = 'developmet'
+    
+config_options = {
+    'development':DevConfig,
+    'production' :ProdConfig
+}     
     
