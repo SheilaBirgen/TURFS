81b94a17b95dd76f728bf0cb78eecd61fc713d46
SheilaBirgen <jeronobergen@gmail.com>
2020-02-20 17:01:32 +0300

    add configs

--- app/models.py
+++ app/models.py
@@ -4,6 +4,12 @@ from . import login_manager
 from datetime import datetime 
 from werkzeug.security import generate_password_hash,check_password_hash
 
+@login_manager.user_loader
+def load_user(user_id):
+    '''
+    reloads the user from the session
+    '''
+    
 class User(db.Model,UserMixin):
     id = db.Column(db.Interger,primary_key=True)
     email = db.Column(db.String(255),unique = True,index = True)
---
+++ config.py
@@ -0,0 +1,40 @@
+import os
+class Config:
+    SECRET_KEY = os.environ.get("SECRET_KEY")
+
+
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    
+
+    class TestConfig(Config):
+
+        pass
+
+
+class ProdConfig(Config):
+    '''
+    Production  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    # SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+
+class DevConfig(Config):
+    '''
+    Development  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    SQLALCHEMY_DATABASE_URI ='postgresql+psycopg2://birgen:admin@localhost/blogpost'
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig,
+} 
